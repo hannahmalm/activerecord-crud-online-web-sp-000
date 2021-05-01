3e7471d162f2bbf7ef451b68ad8d15df2e28af16
Hannah M <hannahemalm616@gmail.com>
2021-05-01 15:27:18 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -28,7 +28,7 @@ def can_be_created_in_a_block(args = {:title => "Home Alone", :release_date => 1
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
-  Movie.create(:title, :release_date) do |m|
+  Movie.create(args) do |m|
   end
 end
 
